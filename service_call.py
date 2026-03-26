7e014b71ec07447cad29f4c3891f266feae3e1a7
Jen Suzuki <jsu@Jens-MacBook-Pro.local>
2026-03-26 15:48:48 -0600

    first script

---
+++ service_call.py
@@ -0,0 +1,9 @@
+job = input("What is the job? ")
+risk = input("What happens if they don’t do it? ")
+reward = input("What’s the benefit of doing it? ")
+
+print("\n--- Service Call Script ---\n")
+print(f"Based on your vehicle, we recommend {job}.")
+print(f"If we leave this as is, {risk}.")
+print(f"Taking care of this today will {reward}.")
+
